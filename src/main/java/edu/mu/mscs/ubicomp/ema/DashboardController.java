fef99fba524e9d325c5b19bd7a5d7efe5e9f1c95
Kowser <kowsercse@gmail.com>
2014-03-12 16:49:37 -0500

    #1: index page

---
+++ src/main/java/edu/mu/mscs/ubicomp/ema/DashboardController.java
@@ -0,0 +1,14 @@
+package edu.mu.mscs.ubicomp.ema;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class DashboardController {
+
+  @RequestMapping("/index.html")
+  public String greeting() {
+    return "index";
+  }
+
+}
---
+++ src/main/webapp/WEB-INF/views/index.jsp
@@ -0,0 +1,10 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+    <title>EMA :: Notifier Web Application</title>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+</head>
+<body>
+EMA Notifier web app
+</body>
+</html>
