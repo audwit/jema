a71768c26223fb100c5d1c3b4c30908da53b8237
Kowser <kowsercse@gmail.com>
2014-03-12 19:21:18 -0500

    #1: moved the DashboardController to controller package

--- src/main/java/edu/mu/mscs/ubicomp/ema/controller/DashboardController.java
+++ src/main/java/edu/mu/mscs/ubicomp/ema/controller/DashboardController.java
@@ -1,4 +1,4 @@
-package edu.mu.mscs.ubicomp.ema;
+package edu.mu.mscs.ubicomp.ema.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
