2d813884f7144e72a3f1b7044fcf513f00790c91
Kowser <kowsercse@gmail.com>
2014-03-14 15:39:31 -0500

    the schedule repository

---
+++ src/main/java/edu/mu/mscs/ubicomp/ema/dao/ScheduleRepository.java
@@ -0,0 +1,31 @@
+package edu.mu.mscs.ubicomp.ema.dao;
+
+import edu.mu.mscs.ubicomp.ema.entity.Schedule;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+import java.util.Date;
+import java.util.List;
+
+@Repository
+public class ScheduleRepository {
+
+  @PersistenceContext
+  private EntityManager entityManager;
+
+  public Schedule find(final Integer id) {
+    return entityManager.find(Schedule.class, id);
+  }
+
+  public List<Schedule> findSchedule(final Date date) {
+    final TypedQuery<Schedule> query = entityManager.createQuery(
+        "SELECT s FROM Schedule s WHERE s.startDate = :today",
+        Schedule.class
+    ).setParameter("today", date);
+
+    return query.getResultList();
+  }
+
+}
