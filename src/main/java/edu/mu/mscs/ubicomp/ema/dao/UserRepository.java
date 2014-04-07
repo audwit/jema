0596a3fc257998463ab8b0c232b0684b84d3bc97
Kowser <kowsercse@gmail.com>
2014-04-06 20:17:23 -0500

    #1: UserRepository class

---
+++ src/main/java/edu/mu/mscs/ubicomp/ema/dao/UserRepository.java
@@ -0,0 +1,26 @@
+package edu.mu.mscs.ubicomp.ema.dao;
+
+import edu.mu.mscs.ubicomp.ema.entity.User;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+import java.util.Date;
+import java.util.List;
+
+@Repository
+public class UserRepository {
+  private static final String QL_STRING = "FROM User u WHERE u.lastLogin > :lastLoginStart AND u.lastLogin < :lastLoginEnd";
+
+  @PersistenceContext
+  private EntityManager entityManager;
+
+  public List<User> getInactiveUsers(final Date lastLoginStart, final Date lastLoginEnd) {
+    final TypedQuery<User> query = entityManager.createQuery(QL_STRING, User.class)
+        .setParameter("lastLoginStart", lastLoginStart)
+        .setParameter("lastLoginEnd", lastLoginEnd);
+    return query.getResultList();
+  }
+
+}
