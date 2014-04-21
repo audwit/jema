73110b9d7d3fe1e53a51a97aed418ad06c7e9261
Kowser <kowsercse@gmail.com>
2014-04-21 15:53:43 -0500

    use jdk time api to calculate time

--- src/main/java/edu/mu/mscs/ubicomp/ema/service/NotificationGeneratorService.java
+++ src/main/java/edu/mu/mscs/ubicomp/ema/service/NotificationGeneratorService.java
@@ -6,7 +6,7 @@ import edu.mu.mscs.ubicomp.ema.entity.ContactingTime;
 import edu.mu.mscs.ubicomp.ema.entity.Notification;
 import edu.mu.mscs.ubicomp.ema.entity.Schedule;
 import edu.mu.mscs.ubicomp.ema.entity.User;
-import org.apache.commons.collections.CollectionUtils;
+import edu.mu.mscs.ubicomp.ema.util.DateTimeUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,18 +15,17 @@ import org.springframework.stereotype.Service;
 import javax.transaction.Transactional;
 import java.sql.Time;
 import java.text.ParseException;
-import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 import java.util.Random;
-import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
 
 @Service
 @Transactional
 public class NotificationGeneratorService {
-  private static final SimpleDateFormat ZERO_TIME_FORMATTER = new SimpleDateFormat("dd/MM/yyyy");
   private Logger logger = LoggerFactory.getLogger(getClass());
 
   public static final int TOTAL_TIME_SLOT = 20;
@@ -39,20 +38,15 @@ public class NotificationGeneratorService {
   private final Random random = new Random();
 
   public void generate() throws ParseException {
-    final Date now = new Date();
-    final Date today = ZERO_TIME_FORMATTER.parse(ZERO_TIME_FORMATTER.format(now));
+    final LocalDateTime now = LocalDateTime.now();
+    final LocalDate today = now.toLocalDate();
     logger.debug("Started notification generation at: {}", today);
 
-    final List<Schedule> schedules = scheduleRepository.findSchedule(today);
+    final List<Schedule> schedules = scheduleRepository.findSchedule(DateTimeUtils.toDate(today));
     logger.debug("Total schedules found: " + schedules.size());
-    if (CollectionUtils.isNotEmpty(schedules)) {
-      schedules
-          .stream()
-          .collect(Collectors.groupingBy(Schedule::getUser))
-          .forEach(this::generateNotification);
-    } else {
-      logger.debug("No schedule found to generate notification.");
-    }
+    schedules.stream()
+        .collect(Collectors.groupingBy(Schedule::getUser))
+        .forEach(this::generateNotification);
   }
 
   private void generateNotification(final User user, final List<Schedule> schedules) {
@@ -62,13 +56,14 @@ public class NotificationGeneratorService {
       return;
     }
     logger.debug("Generating notifications ContactTime: {}", contactingTime);
+    final LocalTime startTime = contactingTime.getStartTime().toLocalTime();
     final int usableSlot = TOTAL_TIME_SLOT - schedules.size() * 3;
     int usedSlot = 0;
     int i = 0;
     for (Schedule schedule : schedules) {
       final int randomSlot = random.nextInt(usableSlot - usedSlot);
       final int baseSlot = usedSlot + randomSlot + i * 3;
-      final Time baseTime = addMinute(contactingTime.getStartTime(), baseSlot * SLOT_DURATION);
+      final LocalTime baseTime = startTime.plusMinutes(baseSlot * SLOT_DURATION);
       logger.debug("Randomized notification start time: {}", baseTime);
 
       final List<Notification> notifications = createNotifications(baseTime, schedule);
@@ -79,26 +74,20 @@ public class NotificationGeneratorService {
     }
   }
 
-  private List<Notification> createNotifications(final Time baseTime, final Schedule schedule) {
+  private List<Notification> createNotifications(final LocalTime baseTime, final Schedule schedule) {
     final List<Notification> notifications = new ArrayList<>();
     for (int i = 0; i < 3; i++) {
-      final Time startTime = addMinute(baseTime, SLOT_DURATION * i);
+      final LocalTime startTime = baseTime.plusMinutes(SLOT_DURATION * i);
 
       final Notification notification = new Notification();
       notification.setSchedule(schedule);
       notification.setSent(false);
-      notification.setScheduledTime(startTime);
+      notification.setScheduledTime(Time.valueOf(startTime));
       notification.setSerial(i);
       notifications.add(notification);
     }
 
     return notifications;
-  }
-
-  private Time addMinute(final Time time, final int minute) {
-    final long time1 = time.getTime();
-    final long time2 = TimeUnit.MINUTES.toMillis(minute);
-    return new Time(time1 + time2);
   }
 
 }
