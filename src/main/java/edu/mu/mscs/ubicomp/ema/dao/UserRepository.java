5998a014135c5f0b45b0c52b0d5e7f3072f4747c
Kowser <kowsercse@gmail.com>
2014-04-29 05:12:58 -0500

    get username to send email

--- src/main/java/edu/mu/mscs/ubicomp/ema/dao/UserRepository.java
+++ src/main/java/edu/mu/mscs/ubicomp/ema/dao/UserRepository.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
 import javax.persistence.TypedQuery;
 import javax.transaction.Transactional;
 import java.util.Date;
@@ -28,5 +29,24 @@ public class UserRepository {
   public void update(final User user) {
     entityManager.merge(user);
     entityManager.flush();
+  }
+
+  public String getName(final User user) {
+    final Query query = entityManager.createNativeQuery("SELECT ec.firstname as firstName, ec.lastname as lastName " +
+        "FROM enrollment_contact ec " +
+        "left join enrolled_user eu ON eu.screening_id = ec.screening_id " +
+        "left join user u ON u.name = eu.study_id " +
+        "where u.id = ?")
+        .setParameter(1, user.getId());
+
+    final String name;
+    final Object[] singleResult = (Object[]) query.getSingleResult();
+    if(singleResult != null) {
+      name = singleResult[0] + " " + singleResult[1];
+    }
+    else {
+      name = user.getUsername();
+    }
+    return name;
   }
 }
