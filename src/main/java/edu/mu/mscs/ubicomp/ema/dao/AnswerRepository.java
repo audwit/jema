c0702835a7fb6816af04a1f60ec4a3175d27bc91
Kowser <kowsercse@gmail.com>
2014-06-03 15:20:21 -0500

    retrieve total submitted answer

---
+++ src/main/java/edu/mu/mscs/ubicomp/ema/dao/AnswerRepository.java
@@ -0,0 +1,28 @@
+package edu.mu.mscs.ubicomp.ema.dao;
+
+import edu.mu.mscs.ubicomp.ema.entity.User;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+import java.util.Date;
+
+@Repository
+public class AnswerRepository {
+
+  @PersistenceContext
+  private EntityManager entityManager;
+
+
+  public int findTotalAnswer(final User user, final Date start, final Date end) {
+    final String totalAnswer = "SELECT distinct (a.submissionTime) FROM Answer a " +
+        "where a.user = :user and a.submissionTime between :startDateTime and :endDateTime";
+    final TypedQuery<Date> query = entityManager.createQuery(totalAnswer, Date.class)
+        .setParameter("user", user)
+        .setParameter("startDateTime", start)
+        .setParameter("endDateTime", end);
+
+    return query.getResultList().size();
+  }
+}
