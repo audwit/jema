0fdefbbc1bd9e031ea884f0b22598fea420236cc
Kowser <kowsercse@gmail.com>
2014-06-17 02:41:12 -0500

    refactor

--- src/main/java/edu/mu/mscs/ubicomp/ema/service/SurveyReminderScheduler.java
+++ src/main/java/edu/mu/mscs/ubicomp/ema/service/SurveyReminderScheduler.java
@@ -10,7 +10,7 @@ import org.slf4j.LoggerFactory;
 
 import javax.annotation.PostConstruct;
 import javax.mail.MessagingException;
-import java.time.LocalDateTime;
+import java.time.LocalDate;
 import java.util.Date;
 import java.util.List;
 import java.util.concurrent.ExecutorService;
@@ -107,9 +107,9 @@ public class SurveyReminderScheduler {
   }
 
   private void findUnopenedSurvey(final int surveyDay, final String surveyType, final String notificationMail) {
-    final LocalDateTime now = LocalDateTime.now();
+    final LocalDate now = LocalDate.now();
     final int totalDay = surveyDay + surveyInactiveDay;
-    final LocalDateTime startLocalDateTime = now.minusDays(totalDay);
+    final LocalDate startLocalDateTime = now.minusDays(totalDay);
     final Date startDate = DateTimeUtils.toDate(startLocalDateTime);
     final List<User> users = userRepository.getRequiresNotificationUsers(surveyType, startDate);
     logger.debug("Found total: {} user to remind for: {} with start date: ", users.size(), surveyType, startLocalDateTime.toString());
