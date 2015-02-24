795a34f428d32f55fefb4e183109a076c91feeda
Kowser <kowsercse@gmail.com>
2015-02-23 23:43:02 -0600

    get submitted survey count for participants

--- src/main/java/edu/mu/mscs/ubicomp/ema/dao/UserRepository.java
+++ src/main/java/edu/mu/mscs/ubicomp/ema/dao/UserRepository.java
@@ -1,6 +1,7 @@
 package edu.mu.mscs.ubicomp.ema.dao;
 
 import edu.mu.mscs.ubicomp.ema.entity.User;
+import org.apache.commons.collections.CollectionUtils;
 import org.apache.commons.lang3.Validate;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -8,6 +9,7 @@ import org.springframework.stereotype.Repository;
 
 import javax.persistence.*;
 import javax.transaction.Transactional;
+import java.util.Collections;
 import java.util.Date;
 import java.util.LinkedList;
 import java.util.List;
@@ -67,6 +69,23 @@ public class UserRepository {
       name = user.getUsername();
     }
     return name;
+  }
+
+  public List<Integer[]> getSubmittedSurveyTotal(final int month, final Date startDate) {
+    final String countSurveyCompletionQuery = "select study_id, survey_id, count(survey_id) from completion " +
+        "where time_stamp = :month " +
+        "and survey_id in (SELECT survey_id FROM Schedule WHERE threemonths = 1 ORDER BY survey_id) " +
+        "and study_id in (select u.name from user u) " +
+        "and study_id in (select u.name from user u where date(u.start_date) = :startDate) " +
+        "group by study_id, survey_id " +
+        "order by survey_id";
+
+
+    final Query query = entityManager.createNativeQuery(countSurveyCompletionQuery)
+        .setParameter("startDate", startDate, TemporalType.DATE)
+        .setParameter("month", month);
+    final List<Integer[]> resultList = (List<Integer[]>)query.getResultList();
+    return CollectionUtils.isNotEmpty(resultList) ? resultList: Collections.emptyList();
   }
 
   public List<User> getRequiresNotificationUsers(Date startDate, String month) {
